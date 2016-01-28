30cc8171ad7f0aa75571c889ee34a6cc66600d3c
Tyrel Tachibana <ttachibana@uco.edu>
2016-01-27 18:06:36 -0600

    Mock-up

---
+++ SoftwareEngineeringProject/SoftwareEngineeringProject/Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SoftwareEngineeringProject
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(Page.IsPostBack)
+            {
+                String temp = DropDownList_Starting_Location.Text;
+                String temp2 = DropDownList_Ending_Location.Text;
+                TextBox_Directions.Text = temp + "\n" + temp2;
+            }
+        }
+
+        protected void Button_Get_Directions_Click(object sender, EventArgs e)
+        {
+            //Server.Transfer("Default.aspx");
+            Response.Redirect("Default.aspx");
+        }
+    }
+}
