148109c31efb03285a726ca28bdef295cf37661c
Drew Demechko <drew_demechko@yahoo.com>
2016-04-16 11:47:18 -0500

    Added all controller routing for team to start creating Views.

---
+++ UCOGPS/src/UCOGPS/Controllers/LoginSystemController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UCOGPS.Controllers
+{
+    public class LoginSystemController : Controller
+    {
+        // /Register
+        [Route("Register")]
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // /Login
+        [Route("Login")]
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ UCOGPS/src/UCOGPS/Controllers/MappingSystemController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace UCOGPS.Controllers
+{
+    public class MappingSystemController : Controller
+    {
+        [Route("CampusMAP")]
+        public IActionResult CampusMap()
+        {
+            return View();
+        }
+    }
+}
