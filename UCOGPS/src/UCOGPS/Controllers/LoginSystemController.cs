309986028aa33c07e491a8ddcdb7941923862f48
Drew Demechko <drew_demechko@yahoo.com>
2016-04-19 23:49:58 -0500

    Finished Register/Login/Logout functionality. LoginSystemController completed.

--- UCOGPS/src/UCOGPS/Controllers/LoginSystemController.cs
+++ UCOGPS/src/UCOGPS/Controllers/LoginSystemController.cs
@@ -1,18 +1,45 @@
-﻿using Microsoft.AspNet.Mvc;
+﻿using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UCOGPS.Models;
 
 namespace UCOGPS.Controllers
 {
     public class LoginSystemController : Controller
     {
+        private AppDbContext _context;
+        public LoginSystemController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         // /Register
         [Route("Register")]
         [HttpGet]
         public IActionResult Register()
         {
+            return View();
+        }
+
+        [Route("Register")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(User user)
+        {
+            if(ModelState.IsValid)
+            {
+                var newUser = _context.User.Where(model => model.Username.Equals(user.Username)).FirstOrDefault();
+
+                if(newUser == null)
+                {
+                    _context.Add(user);
+                    _context.SaveChanges();
+                }
+                return RedirectToAction("Login");
+            }
             return View();
         }
 
@@ -22,6 +49,30 @@ namespace UCOGPS.Controllers
         public IActionResult Login()
         {
             return View();
+        }
+
+        [Route("Login")]
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+
+            if(ModelState.IsValid)
+            {
+                var currentUser = _context.User.Where(model => model.Username.Equals(user.Username)
+                    && model.Password.Equals(user.Password)).FirstOrDefault();
+
+                if(currentUser != null)
+                {
+                    HttpContext.Session.SetString("Username", currentUser.Username.ToString());
+                }
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+        public IActionResult Logout()
+        {
+            HttpContext.Session.SetString("Username", "");
+            return RedirectToAction("Index", "Home");
         }
     }
 }
