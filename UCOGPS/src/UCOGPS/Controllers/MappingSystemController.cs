75add1a14738af4c4b45dcbcd4a853403ce17460
Drew Demechko <drew_demechko@yahoo.com>
2016-04-23 02:34:10 -0500

    Added Json logic for processing directions. Without Google Maps API this has been untested.

--- UCOGPS/src/UCOGPS/Controllers/MappingSystemController.cs
+++ UCOGPS/src/UCOGPS/Controllers/MappingSystemController.cs
@@ -3,15 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using UCOGPS.Models;
 
 namespace UCOGPS.Controllers
 {
     public class MappingSystemController : Controller
     {
+        private AppDbContext _context;
+
+        public MappingSystemController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         [Route("CampusMAP")]
         public IActionResult CampusMap()
         {
             return View();
+        }
+
+        public IActionResult fromLocation(string buildingName)
+        {
+            return getLocation(buildingName);
+        } 
+
+        public IActionResult toLocation(string buildingName)
+        {
+            return getLocation(buildingName);
+        }
+
+        public IActionResult getLocation(string buildingName)
+        {
+            var building = _context.Building.Where(model => model.Name.Equals(buildingName)).FirstOrDefault();
+
+            if (building != null)
+                return Json(new { lat = building.Latitude, lng = building.Longitude });
+            else
+                //notify user of invalid startBuilding
+                return HttpNotFound(building);
         }
     }
 }
