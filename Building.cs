fe07af7401f5d7a4ab9e22c82094dcea0b89ece4
micmac405 <mmckinnon3@uco.edu>
2016-04-23 01:14:25 -0500

    Added files via upload

---
+++ Building.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UCOGPS.Models
+{
+   public class Building
+   {
+      [Key]
+      public int BuildingId { get; set; }
+      public string Name { get; set; }
+      public string Latitude { get; set; }
+      public int Longitude { get; set; }
+   }
+}
